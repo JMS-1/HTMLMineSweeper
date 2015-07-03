ec68299574874dd83395ed6938b1cc27bffbaa6d
JMS-1 <dev-2@psimarron.net>
2015-07-03 18:01:35 +0200

    MVC base complete

--- Schritt3/index.js
+++ Schritt3/index.js
@@ -1,15 +1,18 @@
 ﻿"use strict";
 
 function starten() {
+    // So markieren wir in der Anzeige alle unsere Spielfelder
     var spielfelder = document.querySelectorAll('.mineSweeper');
 
     for (var i = 0; i < spielfelder.length; i++) {
+        // Über die Attribute in der Anzeige werden die Eckdaten des Spielfelds ermittelt
         var spielfeld = spielfelder[i];
 
         var breite = parseInt(spielfeld.getAttribute('data-breite'));
         var hoehe = parseInt(spielfeld.getAttribute('data-hoehe'));
         var minen = parseInt(spielfeld.getAttribute('data-minen'));
 
+        // Ein neues Modell eines Spielfelds wird schließlich mit der Anzeige verbunden
         ControllerSpielfeld.connect(new Spielfeld(breite, hoehe, minen), spielfeld);
     }
 }
--- Schritt3/spielfeld.js
+++ Schritt3/spielfeld.js
@@ -1,5 +1,20 @@
 ﻿"use strict";
 
+/*
+  Jedes Objekt dieser Klasse repräsentiert ein ganzes Spielfeld. Die Dimensionen
+  werden dabei bei der Erzeugung vorgegeben und stehen dann als Informationen zur
+  Verfügung:
+        breite: number          die Anzahl der Zellen pro Zeile des Spielfelds
+        hoehe: number           die Anzahl der Zeilen des Spielfelds
+        minen: number           die Anzahl der Zellen, unter denen Minen versteckt sind.
+
+  Weitere Informationen beschreiben den Zustand des Spiels:
+        zellen: Zelle[]         alles Zellen, die zum Spiel gehören, angeordnet von rechts nach links und dann von oben nach unten
+        zuPrüfen: number        die Anzahl der Zellen ohne Minen, die noch nicht aufgedeckt wurden
+
+  Änderungen zum Spielverlauf werden über Benachrichtigungen mitgeteilt:
+        fertig: (gewonnen: boolean) => void        wird aufgerufen, wenn das Spiel beendet ist - entweder, weil alle Zellen ohne Minen aufgedeckt sind oder versucht wurde, eine Zelle mit Mine aufzudecken
+*/
 function Spielfeld(breite, hoehe, minen) {
     this.breite = breite;
     this.hoehe = hoehe;
@@ -8,14 +23,17 @@ function Spielfeld(breite, hoehe, minen) {
     this.initialisieren();
 }
 
+// Baut den Spielstand völlig neu auf.
 Spielfeld.prototype.initialisieren = function () {
     this.zuPrüfen = this.breite * this.hoehe - this.minen;
     this.zellen = [];
 
+    // Modelle für die Zellen erstellen und merken
     for (var zeile = 0; zeile < this.hoehe; zeile++)
         for (var spalte = 0; spalte < this.breite; spalte++)
             this.zellen.push(new Zelle(this, zeile, spalte));
 
+    // Die gewünschte Anzahl von Minen verstecken
     for (var zuVerstecken = this.minen; zuVerstecken-- > 0;) {
         var zelle = this.zufallsZelle();
         if (zelle.istMine)
@@ -27,6 +45,7 @@ Spielfeld.prototype.initialisieren = function () {
     this.fertig = null;
 }
 
+// Sucht eine Zelle ohne Mine und ohne Minen im direkten Umfeld und führt die Prüfung darauf durch.
 Spielfeld.prototype.freiesFeldSuchen = function () {
     for (; ;) {
         var zelle = this.zufallsZelle();
@@ -39,6 +58,7 @@ Spielfeld.prototype.freiesFeldSuchen = function () {
     }
 }
 
+// Ermittelt zu einer Zelle die Minen im direkten Umfeld.
 Spielfeld.prototype.minenZählen = function (zelle) {
     var minen = 0;
 
@@ -50,32 +70,36 @@ Spielfeld.prototype.minenZählen = function (zelle) {
     return minen;
 }
 
+// Ermittelt eine zufällige Zelle.
 Spielfeld.prototype.zufallsZelle = function () {
     return this.zellen[Math.floor(Math.random() * this.zellen.length)];
 }
 
+// Beendet das Spiel als verloren.
 Spielfeld.prototype.verloren = function () {
     this.prüfen(-1);
 }
 
+// Vermerkt, dass eine weiter Zelle ohne Mine aufgedeckt wurde.
 Spielfeld.prototype.erfolgreichGeprüft = function () {
     this.prüfen(this.zuPrüfen - 1);
 }
 
-Spielfeld.prototype.istBeendet = function () {
-    return (this.zuPrüfen < 1);
-}
-
+// Aktualisiert den Spielstand.
 Spielfeld.prototype.prüfen = function (zuPrüfen) {
+    // Spielstand übernehmen
     this.zuPrüfen = zuPrüfen;
 
-    if (!this.istBeendet())
+    // Es gibt noch Zellen, die aufgedeckt werden können
+    if (this.zuPrüfen > 0)
         return;
 
+    // Alle Zellen als beendet markieren und so weitere Aktionen des Anwenders unterbinden
     this.zellen.forEach(function (zelle) {
         zelle.beenden();
     });
 
+    // Das Ende des Spiels mitteilen und dabei die Anzeige aktualisieren - das ist nicht Aufgabe des Modells!
     if (this.fertig !== null)
         this.fertig(zuPrüfen == 0);
 }
